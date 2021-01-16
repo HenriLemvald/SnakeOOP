ea2cd5e887205dce1883adadc23f6629b5e90b44
henrilemvald <henrilemvald@gmail.com>
2021-01-16 15:46:24 +0200

    first commit

---
+++ SnakeOOP/Walls.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeOOP
+{
+    class Walls
+    {
+        List<Figure> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+            HorizontalLine top = new HorizontalLine(0, 80, 0, '#');
+            VerticalLine left = new VerticalLine(0, 25, 0, '#');
+            HorizontalLine bottom = new HorizontalLine(0, 80, 25, '$');
+            VerticalLine right = new VerticalLine(0, 25, 80, '$');
+
+            wallList.Add(top);
+            wallList.Add(left);
+            wallList.Add(bottom);
+            wallList.Add(right);
+        }
+
+        public void Draw()
+        {
+            foreach(var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+
+        public bool IsHit(Figure figure)
+        {
+            foreach(var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
